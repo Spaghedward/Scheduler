4a36a9f34141d232e1975c2aba519e114acafffc
Gordon <gordonjackman@gmail.com>
2023-11-20 21:25:05 -0500

    I was too cute with the local storage and trying to use a for loop to display the client side storage.  Making the key value of the local storage the time, I was able to do it way cleaner and probably more streamlined.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Scheduler
+
+## Image
+![Alt text](image.png)
+
+## Description
+
+This was an excercise in javascript logic.  A script was written to create a scheduler that dynamically changed based on time of day.  It also stored values into local storage so they would be present after refresh.
+
+## Installation
+
+Stylesheet and script are located in the assets folder.
+
+## Licensing
+
+MIT
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,21 @@
+// This added the current date to the top of the page.
 var currentDay = dayjs().format('dddd, MMMM D');
 $('#currentDay').text(currentDay);
 
+// Added an event listener that stores user inputs and the corresponding hour
+// the input was written in.
 $('.saveBtn').on('click', storeInput)
 
 function storeInput() {
     var userInput = $(this).siblings('.description').val();
     var calendarTime = $(this).parent().attr('id');
     var currentTime = dayjs().hour();
-    console.log(currentTime);
-    console.log(calendarTime);
-    var existingData = JSON.parse(localStorage.getItem('dataEntered'))??[];
-    var newData = existingData.concat(
-        [{userInput,
-          calendarTime, 
-        }
-        ]
-    );
-    localStorage.setItem('dataEntered', JSON.stringify(newData));
-
-   
-
+    // console.log(currentTime);
+    // console.log(calendarTime);
+    localStorage.setItem(calendarTime, userInput);
+ 
+// The if statement dynamically changes the color of the description based on
+// whether the task is in the past, present or future.
     if (calendarTime < currentTime) {
         $(this).parent().removeClass('present future').addClass('past');
     } else if (calendarTime == currentTime) {
@@ -30,35 +26,15 @@ function storeInput() {
 
 }
 
-// $('#9 .description').val(localStorage.getItem('userInput'));
-// $('#10 .description').val(localStorage.getItem('userInput'));
-// $('#11 .description').val(localStorage.getItem('userInput'));
-// $('#12 .description').val(localStorage.getItem('userInput'));
-// $('#13 .description').val(localStorage.getItem('userInput'));
-// $('#14 .description').val(localStorage.getItem('userInput'));
-// $('#15 .description').val(localStorage.getItem('userInput'));
-// $('#16 .description').val(localStorage.getItem('userInput'));
-// $('#17 .description').val(localStorage.getItem('userInput'));
+// The saved local storage objects stay on the page based on what hour it was entered in.cd ..
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
 
-// storeInput()
-
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  ;
+storeInput()
